4a9ecedf5c638f6dd4d2663a2155be3c8c9741af
anto <antonioverascastillo@gmail.com>
2024-04-10 23:15:37 -0400

    set y get done

--- FinalAlgrtClsc/src/logico/Grafo.java
+++ FinalAlgrtClsc/src/logico/Grafo.java
@@ -1,5 +1,27 @@
 package logico;
 
+import java.util.ArrayList;
+
 public class Grafo {
-//
+	
+	 private ArrayList<Nodo> misNodos;
+	 private ArrayList<Arista> misAristas;
+	 
+	 
+	public ArrayList<Nodo> getMisNodos() {
+		return misNodos;
+	}
+	
+	public void setMisNodos(ArrayList<Nodo> misNodos) {
+		this.misNodos = misNodos;
+	}
+	
+	public ArrayList<Arista> getMisAristas() {
+		return misAristas;
+	}
+	
+	public void setMisAristas(ArrayList<Arista> misAristas) {
+		this.misAristas = misAristas;
+	}
+
 }
